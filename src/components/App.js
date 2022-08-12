08de0c22cd847e526c657448021b47f8a80a17bc
Ganit kumar <104363798+ganitkumar517@users.noreply.github.com>
2022-08-12 15:26:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,6 @@ function App(){
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('ball'));
 root.render(<App />);
 export default App;
